a94afe8343bb420aee1f0e41cae6fdc6bfa27caa
Harishsbb <bavaharishkumar@gmail.com>
2026-02-19 15:41:01 +0530

    Removed old project files

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="bg-secondary p-4 mt-8 text-center">
-            <p>&copy; 2024 Harish K. All rights reserved.</p>
-        </footer>
-    );
-};
-
-export default Footer;
